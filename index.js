2d5265b96ed4413edcf39e0f8c591fbb9338ff75
Milena Fontenele <milenfontenele@hotmail.com>
2023-11-02 21:45:33 -0300

    add inicial projeto

---
+++ index.js
@@ -0,0 +1,12 @@
+let pokemon = [
+    ["POOCHYENA", 2, "M"],
+    ["ZIGZAGOON", 2, "F"],
+    ["DRAGONITE", 5, "M"],
+    ["POOCHYENA", 3, "F"],
+]
+
+console.log(pokemon[0][0] + ' é do sexo ' + pokemon[0][2] + ' está no nível ' + pokemon[0][1] + ' e foi cadastrado com sucesso!');
+console.log(pokemon[1][0] + ' é do sexo ' + pokemon[1][2] + ' está no nível ' + pokemon[1][1] + ' e foi cadastrado com sucesso!');
+console.log(pokemon[2][0] + ' é do sexo ' + pokemon[2][2] + ' está no nível ' + pokemon[2][1] + ' e foi cadastrado com sucesso!');
+console.log(pokemon[3][0] + ' é do sexo ' + pokemon[3][2] + ' está no nível ' + pokemon[3][1] + ' e foi cadastrado com sucesso!');
+
